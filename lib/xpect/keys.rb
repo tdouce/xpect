c62e1e020aeb0e58ecf380250dc7124db3fd60c9
Travis Douce <travis.douce@stitchfix.com>
2018-01-21 23:36:52 -0500

    Refactor Xpect::Keys
    This makes it a bit more readable although a little more verbose.

--- lib/xpect/keys.rb
+++ lib/xpect/keys.rb
@@ -7,35 +7,42 @@ module Xpect
       @required = required
     end
 
-    # Return only what is specified in spec?
     def conform!(value:, path: [])
-      reduced = reduce(enumerable: @required, init: {}, value: value, path: path,
-                       when_value_does_not_have_key: lambda do |memo, val, key, path|
-                         if val.is_a?(Pred) && val.default
-                           memo[key] = val.default
-                         else
-                           raise FailedSpec, "does not include '#{ key }' at '#{ path }'"
-                         end
-
-                         memo
-                       end)
-
-      reduce(enumerable: @optional, init: reduced, value: value, path: path)
+      required = process_required(value, path)
+      process_optional(required, value, path)
     end
 
     private
 
-    def reduce(enumerable:, init:, value:, path:, when_value_does_not_have_key: nil)
-      enumerable.reduce(init) do |memo, (key, val)|
+    def process_optional(init, value, path)
+      @optional.reduce(init) do |memo, (key, val)|
         if val.is_a?(Keys)
           memo[key] = val.conform!(value: value[key], path: path)
         else
           if value.has_key?(key)
             data_value = value.fetch(key)
             memo[key] = Xpect::Type.process(val, val, data_value, path)
-            
-          elsif when_value_does_not_have_key
-            memo = when_value_does_not_have_key.call(memo, val, key, path)
+          end
+        end
+
+        memo
+      end
+    end
+
+    def process_required(value, path)
+      @required.reduce({}) do |memo, (key, val)|
+        if val.is_a?(Keys)
+          memo[key] = val.conform!(value: value[key], path: path)
+        else
+          if value.has_key?(key)
+            data_value = value.fetch(key)
+            memo[key] = Xpect::Type.process(val, val, data_value, path)
+          else
+            if val.is_a?(Pred) && val.default
+              memo[key] = val.default
+            else
+              raise FailedSpec, "does not include '#{ key }' at '#{ path }'"
+            end
           end
         end
 
